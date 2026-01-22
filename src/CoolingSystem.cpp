fe7be5601af798f74b038da694d27f04b70818e4
Ethan D Bauer <81826299+MonkeyTitan@users.noreply.github.com>
2026-01-22 16:22:30 -0600

    Delete src directory

--- src/CoolingSystem.cpp
+++
@@ -1,5 +0,0 @@
-/*
-This is where we will eventually implement the actual logic we want for each "async" task. Here we have the CoolingSystem.cpp file. This houses our
-class for the cooling system logic and we can define a time interval it opperates on so we don't have to worry about a blockage. This file calls it's header for
-the needed variable and function definitions. 
- */

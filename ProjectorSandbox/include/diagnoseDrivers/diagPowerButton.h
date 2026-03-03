83b924c2ed1afcc1cf21587dac1d11c24655db79
Ben Vrakas <benvrakas@gmail.com>
2026-03-03 13:09:41 -0600

    Removed sandbox

--- ProjectorSandbox/include/diagnoseDrivers/diagPowerButton.h
+++
@@ -1,6 +0,0 @@
-//Functions for testing and recording diagnostic infor related to the power button
-#pragma once
-#include "PowerButton.h"
-#include "BoardPins.h"
-
-void PowerButtonReadOut();
--- ProjectorSandbox/src/diagnoseDrivers/diagPowerButton.cpp
+++
@@ -1,8 +0,0 @@
-#include "diagPowerButton.h"
-
-void PowerButtonReadOut() {
-    Serial.print("Power Button State: ");
-    Serial.print(digitalRead(BoardPins::PIN_SW_BTN) == LOW ? "Pressed" : "Released");
-    Serial.print("Power Button Armed Status: ");
-    Serial.println(powerButton.isArmed());
-}
